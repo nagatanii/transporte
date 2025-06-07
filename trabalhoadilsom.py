2a558cb478688bf5a551c4e0c15c0a427640e1f9
Vinicius Madalosso <viniciusgmm012@gmail.com>
2025-06-06 23:12:15 -0300

    modificações

--- trabalhoadilsom.py
+++ trabalhoadilsom.py
@@ -6,57 +6,70 @@ print('------ Transporte ------')
 
 
 
-#Biblioteca
-import os
-import time
+# Biblioteca para limpar a tela e pausar o programa
+import os           # usada para limpar a tela
+import time         # usada para criar pausas temporais no programa
 
-#variaveis
+# Variáveis globais usadas em diversas funções
 login1 = 0
-v_escolhido = None
-ve = None
-valores = 55.00
-va = 0
+v_escolhido = None  # Armazena o veículo escolhido pelo motorista
+ve = None           # Variável auxiliar para escolha de veículo
+valores = 55.00     # Valor fixo da passagem
 
-#Senhas
+# Dados de login para motorista
 senha_motorista = '1234567'
 login_motoristaa = 'kauan'
 
+# Dados de login para administrador
 senha_administrador = '7654321'
 login_administradorr = 'vinicius'
 
-#Lista de horarios 
-
+# Horários de saída e chegada dos itinerários
 saida = ['06:00', '07:30', '09:00', '10:30', '12:00']
 chegada = ['07:30', '09:00', '10:30', '12:00', '13:30']
 
-#lista de compra da passagem
+# Lista do carrinho de compras
 carrinho = []
-poltronas = [1, 2 ,3 , 4 ,5 , 6, 7, 8, 9, 10]
 
-#Lista de veiculos
+# Poltronas disponíveis
+poltronas = [1, 2, 3, 4 , 5, 6, 7, 8, 9, 10]
+
+# Lista dos veículos
 veiculos = ['Ônibus 1', 'Ônibus 2', 'Micro-ônibus', 'van']
 
+#Lista de Rotas
+rotas = [
+    'Mogi > Guarulhos',
+    'Mogi > São José Dos Campos',
+    'Mogi > Guararema',
+    'Mogi > Santos'
+]
 
-#Funções
+
+# funções
+
+
+# menu do usuário comum
 def m_usuario():
     while True:
-        os.system('cls')
+        os.system('cls')    # limpa tela
         print('-----\033[1;34;40m Menu Usuário\033[m -----\n')
-        print('\033[1;37;40m1. Itinerários')
-        print('\033[1;37;40m2. Comprar Passagem') 
-        print('\033[1;37;40m3. Carrinho')
-        print('\033[1;31;40m4. Sair\033[m')
+        print('\033[1;37;40m1. Itinerários')            # Mostrar horários
+        print('\033[1;37;40m2. Comprar Passagem')       # Comprar poltronas
+        print('\033[1;37;40m3. Carrinho')               # Ver poltronas compradas
+        print('\033[1;31;40m4. Sair\033[m')             # Sair do menu
         
         usu = int(input('\n\033[1;37;40mDigite o número correspondente: '))
 
-        if usu == 1: itinierarios()
-        elif usu == 2: passagem()
-        elif usu == 3: bilhetes() 
+        if usu == 1: itinierarios()             # Chama a função para mostrar itinerários
+        elif usu == 2: passagem()               # Função para comprar passagem
+        elif usu == 3: bilhetes()               # Mostrar carrinho
         elif usu == 4: 
             print('Fechando o Programa')
             os.system('cls')
-            break
+            break   # sai do menu do usuário
 
+# autenticação para perfil de motorista.
 def login_motorista():
     os.system('cls')
     print('---------LOGIN MOTORISTA---------')
@@ -66,9 +79,9 @@ def login_motorista():
         sair = input('Digite 0 para sair ou pressione ENTER para continuar: ')
         if sair == '0':
             print('Fechando...')
-            time.sleep(1)
+            time.sleep(1)       # temporizador (dá uma pausa antes de uma ação)
             os.system('cls')
-            return login()
+            return login()      # Retorna ao menu de login geral
         
 
         loginn =  input('Digite seu login: ')
@@ -80,25 +93,39 @@ def login_motorista():
             login()
 
 
-        if loginn == login_motoristaa and senhaa == senha_motorista:
+        if loginn == login_motoristaa and senhaa == senha_motorista:    # se o login e a senha estiverem corretos, direciona até o menu do motorista
             print ('login \033[32mcorreto!!\033[m Carregando...')
             time.sleep(3)
-            m_motorista()
+            m_motorista()       # Entra no menu motorista
             break
         else: print('login \033[31mincorreto!\033[m Tente outra vez')
         time.sleep(2)
         os.system('cls')
         print('---------LOGIN MOTORISTA---------')
         
-
+# autenticação para perfil de administrador.
 def login_administrador():
     os.system('cls')
     print('---------LOGIN ADMINISTRADOR---------')
     print ('\n')
     while True:
-        login =  input('Digite seu login de usuário: ')
-        senha = input('Digite a sua senha: ')
-        if login == login_administradorr and senha == senha_administrador:
+
+        sair = input('Digite 0 para sair ou pressione ENTER para continuar: ')
+        if sair == '0':
+            print('Saindo...')
+            time.sleep(1)
+            os.system('cls')
+            return login()
+        else: sair == ''
+        os.system('cls')
+        
+        print('---------LOGIN ADMINISTRADOR---------')
+        print ('\n')
+        loginn =  input('Digite seu login: ')
+        senhaa = input('Digite a sua senha: ')
+
+
+        if loginn == login_administradorr and senhaa == senha_administrador:
             print ('login \033[32mcorreto!!\033[m Carregando...')
             time.sleep(3)
             m_admin()
@@ -106,8 +133,10 @@ def login_administrador():
         else: print('login \033[31mincorreto!\033[m Tente outra vez')
         time.sleep(2)
         os.system('cls')
+        print('---------LOGIN ADMINISTRADOR---------')
+        print ('\n')
 
-
+# menu do motorista 
 def m_motorista():
     os.system('cls')
     while True:
@@ -115,40 +144,35 @@ def m_motorista():
         print('----- Menu Motorista -----')
         print('1. Rotas Disponiveis')
         print('2. Veículos Disponiveis') 
-        print('3. Paradas')
-        print('4. Sair')
+        print('3. Sair')
 
         try:
             moto = int(input('Digite o número correspondente ao que Deseja: '))
         except:
             print('Digite Apenas Numeros Válidos!!')
-            time.sleep(2)
-            os.system('cls')
-            continue
+            time.sleep(2)       # temporizador
+            os.system('cls')    # limpa tela
+            continue            # Volta ao início do loop para repetir o menu
         if moto == 1:
             rotas_d()
         elif moto == 2: 
             veiculos_d()
         elif moto == 3: 
-            paradas_d()
-        elif moto == 4:
             print('Fechando o Menu')
             os.system('cls')
-            break
-        login()
+            break               
             
-
-    
+# escolha de veículo pelo motorista
 def veiculos_d(): 
-    os.system('cls') #limpa tela
-    global v_escolhido, ve # puxa as variavéis que estão fora para dentro da função
+    os.system('cls')            #limpa tela
+    global v_escolhido, ve      # puxa as variavéis que estão fora para dentro da função
 
     if v_escolhido is not None: # se v_escolhido for verdadeiro ou true, aparecerá está mensagem dizendo, que o veiculo está armazenado.
         print('---------- Menu veículos ----------')
         print(f"\nVocê já escolheu o veículo: {v_escolhido}")
         print("\nNão é possível escolher novamente.")
-        time.sleep(3)
-        return
+        time.sleep(3)   # temporizador
+        return          # Sai da função, impedindo nova escolha de veículo
 
     while True:
 
@@ -190,95 +214,61 @@ def veiculos_d():
             print("Opção inválida, tente novamente.")
             continue
 
+# exibe as rotas
 def rotas_d():
     os.system('cls')
-    global v_escolhido
-    if v_escolhido is not None:
+    global v_escolhido, rotas       # Usa as variáveis globais 'v_escolhido' e 'rotas' dentro da função
+
+    if v_escolhido is not None:     # Verifica se o motorista já escolheu um veículo
         print(f'seu veiculo escolhido foi {v_escolhido} ')
-        if v_escolhido == veiculos[0]:
-            print('A sua rota é: Mogi > Guarulhos')
-        elif v_escolhido == veiculos[1]:
-            print('A sua rota é: Mogi > São José Dos Campos')
-        elif v_escolhido == veiculos[2]:
-            print('A sua rota é: Mogi > Guararema')
-        elif v_escolhido == veiculos[3]:
-            print('A sua rota é: Mogi > Bertioga')
+        index = None                # Inicializa a variável 'index' com None
+        try:
+            index = veiculos.index(v_escolhido)
+        except ValueError:
+            index = None
+
+        if index is not None and index < len(rotas):
+            print(f'A sua rota é: {rotas[index]}')
+        else:
+            print('Rota não disponível para este veículo.')
     else:
         print('Você ainda não escolheu um veículo.')
 
     print('-------ROTAS DISPONIVEIS-------')
-    print('Rota Ônibus 1: Mogi > Guarulhos')
-    print('Rota Ônibus 2: Mogi > São José Dos Campos')
-    print('Rota Micro-Ônibus: Mogi > Guararema')
-    print('Rota Van: Mogi > Santos')  
+    for i, rota in enumerate(rotas):
+        print(f'Rota {veiculos[i]}: {rota}')
+
     while True:
         tecla = input('Digite 0 para voltar ao menu...: ')
-        if tecla.strip() == '0': # só continua se for exatamente "0"
-            return m_motorista() 
+        if tecla.strip() == '0':
+            return m_motorista()
         else:
             print('tecla invalida, tente novamente!!')
         
-
-def paradas_d():
-    global v_escolhido
+# usado pelo administrador para excluir rotas.
+def remover_rota():
+    global rotas, veiculos
     os.system('cls')
-    if v_escolhido is not None:
-        print(f'seu veiculo escolhido foi {v_escolhido} Suas Paradas São: \n')
-        if v_escolhido == veiculos[0]:
-            print('---------Suzano; Itaim Paulista---------')
-        elif v_escolhido == veiculos[1]:
-            print('---------Guararema; Jacareí---------')
-        elif v_escolhido == veiculos[2]:
-            print('---------Sabaúna---------')
-        elif v_escolhido == veiculos[3]:
-            print('---------Mauá; Cubatão---------')
+    print('--- Remover Rota ---\n')
+
+    if not rotas:
+        print('Nenhuma rota disponível para remoção.\n')
     else:
-        print('Você ainda não escolheu um veículo.')
-        
-        print('-------PARADAS-------')
-        print('|Mogi > guarulhos          | Parada em suzano e itaim paulista')
-        print('|Mogi > são josé dos campos| parada em guararema e jacareí')
-        print('|Mogi > guararema          | parada em sabauna')
-        print('|Mogi > Santos             | parada em mauá e cubatão') 
-    while True:
-        tecla = input('\n\nDigite 0 para voltar ao menu...: ')
-        if tecla.strip() == '0': # só continua se for exatamente "0"
-            return m_motorista() 
-        else:
-            print('tecla invalida, tente novamente!!')
+        for i in range(min(len(rotas), len(veiculos))):
+            print(f'{i + 1}. {veiculos[i]}: {rotas[i]}')
 
-
-def m_admin():
-    while True:
-        os.system('cls')
-        print('----- Menu Administrador -----')
-        print('1. Remover Veículos')
-        print('2. Trocar Rotas') 
-        print('3. Sair')
-
-        adm= int(input('Digite o número correspondente: '))
-
-        if adm == 1: remover_veiculo()
-        elif adm == 2: ''
-        elif adm == 3:  
-            print('Fechando o Programa')
-            os.system('cls')
-            break
-            
-def remover_veiculo():
-    os.system('cls')
-    print('--- Remover Veículo ---\n')
-    
-    if not veiculos:  #Verifica se a lista veiculos está vazia
-        print('Nenhum veículo disponível para remoção.\n')
-    else:
-        for i, v in enumerate(veiculos):
-            print(f'{i + 1}. {v}')
         try:
-            opcao = int(input('\nDigite o número do veículo que deseja remover: '))
-            if 1 <= opcao <= len(veiculos):
-                removido = veiculos.pop(opcao - 1)
-                print(f'\n{removido} foi removido com sucesso.')
+            opcao = int(input('\nDigite o número da rota que deseja remover: '))
+            if 1 <= opcao <= len(rotas):
+                removida = rotas.pop(opcao - 1)
+                veiculo_removido = veiculos.pop(opcao - 1)
+                print(f'\nRota "{removida}" do veículo "{veiculo_removido}" foi removida com sucesso.')
+                
+                # Se o veículo removido for o escolhido pelo motorista, resetar a escolha
+                global v_escolhido
+                if v_escolhido == veiculo_removido:
+                    v_escolhido = None
+                    print('Veículo selecionado pelo motorista foi removido. A escolha foi resetada.')
             else:
                 print('\nOpção inválida.')
         except ValueError:
@@ -286,6 +276,65 @@ def remover_veiculo():
 
     input('\nPressione Enter para voltar ao menu...')
 
+# menu do administrador
+def m_admin():
+    while True:
+        os.system('cls')
+        print('----- Menu Administrador -----')
+        print('1. Remover Veículos')
+        print('2. Remover Rotas') 
+        print('3. Sair')
+
+        try:
+            adm= int(input('Digite o número correspondente: '))
+        except:
+            print('Digite apenas números válidos.')
+            time.sleep(2)
+            continue
+
+        if adm == 1:
+            remover_veiculo()
+        elif adm == 2:
+            remover_rota()
+        elif adm == 3:  
+            print('Fechando o Programa')
+            os.system('cls')
+            break
+        else:
+            print('Opção inválida.')
+            time.sleep(2)
+
+# usado pelo administrador para excluir veiculos.
+def remover_veiculo():
+    global veiculos, rotas
+    os.system('cls')
+    print('--- Remover Veículo ---\n')
+
+    if not veiculos:
+        print('Nenhum veículo disponível para remoção.\n')
+    else:
+        for i, v in enumerate(veiculos):
+            print(f'{i + 1}. {v}')
+
+        try:
+            opcao = int(input('\nDigite o número do veículo que deseja remover: '))
+            if 1 <= opcao <= len(veiculos):
+                removido = veiculos.pop(opcao - 1)
+                rota_removida = rotas.pop(opcao - 1)  # REMOVER a rota também!
+                print(f'\nVeículo "{removido}" e rota "{rota_removida}" foram removidos com sucesso.')
+                
+                global v_escolhido
+                if v_escolhido == removido:
+                    v_escolhido = None
+                    print('Veículo escolhido pelo motorista foi removido.')
+            else:
+                print('\nOpção inválida.')
+        except ValueError:
+            print('\nEntrada inválida. Digite apenas números.')
+
+    input('\nPressione Enter para voltar ao menu...')
+
+# menu de seleção de perfil (usuário, motorista ou administrador).
 def login():
    os.system('cls')
    while True: 
@@ -319,9 +368,8 @@ def login():
             print('Nenhuma Correspondência, Digite Novamente!')
             time.sleep(1)
             os.system('cls')
-
     
-
+# exibe horários de saída e chegada.
 def itinierarios(): #função para mostrar os horários de saída e chegada 
     os.system('cls')
     print('----\033[1;32;40m Itinerários \033[m---\n')
@@ -341,7 +389,7 @@ def itinierarios(): #função para mostrar os horários de saída e chegada
             time.sleep
             os.system('cls')
 
-    
+# simula a compra de passagens e adição ao carrinho.
 def passagem(): #função para colocar poltronas no carrinho, e tambem listar quais estão disponiveis 
     os.system('cls')
     while True:
@@ -369,6 +417,7 @@ def passagem(): #função para colocar poltronas no carrinho, e tambem listar qu
         if continuar != 's':
             break
 
+# exibe o conteúdo do carrinho.
 def bilhetes(): #função para mostrar as poltronas adicionadas ao carrinho
     os.system('cls')
     while True:
